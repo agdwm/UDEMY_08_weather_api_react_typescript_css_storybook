d06d6a3d2316a3f6f484bb37a33dbb3ccaa17e8f
Almudena Guerras De Diego <almudenaguerrasdediego@MacBook-Pro-de-Almudena-2.local>
2026-03-05 13:18:12 +0100

    feat: implement BASIC weather fetching functionality and enhance error handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
+import type { Weather } from "@/types/weather-interface";
+import { useState } from "react";
+
 const App = () => {
+  const [outputWeather, setOutputWeather] = useState(
+    "Haz clic para cargar datos",
+  );
+
+  const fetchWeather = async (city: string): Promise<void> => {
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+    const requestUrl = `${baseUrl}?key=${API_KEY}&q=${encodeURIComponent(city)}&aqi=no`;
+
+    try {
+      setOutputWeather("Cargando datos...");
+
+      const response = await fetch(requestUrl);
+
+      if (!response.ok) {
+        throw new Error(`Error on fetching data (${response.statusText})`);
+      }
+
+      const data: Weather = await response.json();
+      setOutputWeather(JSON.stringify(data, null, 2));
+    } catch (error) {
+      if (error instanceof Error) {
+        setOutputWeather(`Error on fetching data, ${error.message}`);
+      } else {
+        setOutputWeather(`Error desconocido al consultar la API`);
+      }
+    }
+  };
+
   return (
-    <main className="u-container u-py-6">
+    <main className="u-container">
       <section className="u-flow">
-        <h1>02 API Practice</h1>
-        <p className="u-muted">
-          Estructura base preparada con tokens, layers y utilidades
-          reutilizables.
-        </p>
+        <h1>API Practice</h1>
+        <button
+          className="u-btn u-focus-ring"
+          onClick={() => {
+            fetchWeather("Meco, Spain");
+          }}
+        >
+          Consultar tiempo para Meco
+        </button>
+        {outputWeather && <pre>{outputWeather}</pre>}
       </section>
     </main>
   );
