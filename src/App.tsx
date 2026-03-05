d6dad01af13483e1360f3d92d72f111514bb750b
Almudena Guerras De Diego <almudenaguerrasdediego@MacBook-Pro-de-Almudena-2.local>
2026-03-05 13:35:40 +0100

    fix: refactor weather fetching logic and improve error messages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ const App = () => {
   );
 
   const fetchWeather = async (city: string): Promise<void> => {
-    const baseUrl = import.meta.env.VITE_API_BASE_URL;
-    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-    const requestUrl = `${baseUrl}?key=${API_KEY}&q=${encodeURIComponent(city)}&aqi=no`;
+    const apiRootUrl = import.meta.env.VITE_API_BASE_URL;
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const endpointPath = "/current.json";
+    const requestUrl = `${apiRootUrl}${endpointPath}?key=${apiKey}&q=${encodeURIComponent(city)}&aqi=no`;
 
     try {
       setOutputWeather("Cargando datos...");
@@ -17,14 +18,14 @@ const App = () => {
       const response = await fetch(requestUrl);
 
       if (!response.ok) {
-        throw new Error(`Error on fetching data (${response.statusText})`);
+        throw new Error(`${response.status} ${response.statusText}`);
       }
 
       const data: Weather = await response.json();
       setOutputWeather(JSON.stringify(data, null, 2));
     } catch (error) {
       if (error instanceof Error) {
-        setOutputWeather(`Error on fetching data, ${error.message}`);
+        setOutputWeather(`Error al consultar la API: ${error.message}`);
       } else {
         setOutputWeather(`Error desconocido al consultar la API`);
       }
